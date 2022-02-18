fc6b5af7cf3c8da2112c3370001d680c7697dd5f
Derek Douglas <derek.j.douglas13@gmail.com>
2022-02-17 22:32:54 -0600

    finished JS file and commented out console.logs, added education section to HTML

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('hello world');
+/*console.log('hello world');*/
 
 
 let username = prompt('Hello there. What is your name?');
@@ -9,26 +9,66 @@ alert('Ah, just as I thought; ' + username + '. A wonderful name!');
 let game = prompt('Would you like to play a game? All responses should be \'yes\' or \'no\'.').toLowerCase();
 
 if (game === 'yes') {
-  console.log('Eggselent');
+  /*console.log('Eggselent');*/
   alert('Eggselent');
 } else if (game === 'no') {
-  console.log('Too bad! We\'re playing anyway!');
+  /*console.log('Too bad! We\'re playing anyway!');*/
   alert('Too bad! We\'re playing anyway!');
 } else {
-  console.log('Hmm, I don\'t understand. Let\'s play anyways.');
+  /*console.log('Hmm, I don\'t understand. Let\'s play anyways.');*/
   alert('Hmm, I don\'t understand. Let\'s play anyways.');
 }
 
-let military = prompt('Was I in the Navy?');
+let military = prompt('Was I in the Navy?').toLowerCase();
 
 if (military === 'yes') {
-  console.log('Nope. I was in the Air Force');
+  /*console.log('Nope. I was in the Air Force');*/
   alert('Nope. I was in the Air Force.');
 } else if (military === 'no') {
-  console.log('That\'s right! I was in the Air Force.');
+  /*console.log('That\'s right! I was in the Air Force.');*/
   alert('That\'s right! I was in the Air Force.');
 } else {
-  console.log('I see you\'re playing hardball. Let\'s move on.');
+  /*console.log('I see you\'re playing hardball. Let\'s move on.');*/
+  alert('I see you\'re playing hardball. Let\'s move on.');
 }
 
-let hometown = prompt('Was I born in Buffalo, NY?')
+let hometown = prompt('Am I from Western New York?').toLowerCase();
+
+if (hometown === 'yes') {
+  /*console.log('That\'s right! From the southern shores of Lake Ontario.');*/
+  alert('That\'s right! From the southern shores of Lake Ontario.');
+} else if (hometown === 'no') {
+  /*console.log('WHAT?! Do you even know me at all? Of course I am!');*/
+  alert('WHAT?! Do you even know me at all? Of course I am!');
+} else {
+  /*console.log('Don\'t like to follow the rules? That\'s okay.');*/
+  alert('Don\'t like to follow the rules? That\'s okay.');
+}
+
+let peach = prompt('Did I play in peach orchards as a kid?').toLowerCase();
+
+if (peach === 'yes') {
+  /*console.log('I sure did. The climate of the great lakes helps to enable fruit tree orchards in such a cold climate.');*/
+  alert('I sure did. The climate of the great lakes helps to enable fruit tree orchards in such a cold climate.');
+} else if (peach = 'no') {
+  /*console.log('Uhm, I actually did. Awkward.');*/
+  alert('Uhm, I actually did. Awkward.');
+} else {
+  /*console.log('At a loss for words, eh?');*/
+  alert('At a loss for words, eh?');
+}
+
+let daughters = prompt('Do I have three daughters?').toLowerCase();
+
+if (daughters === 'yes') {
+  /*console.log('Not yet, but you never know in the future! The answer is two.');*/
+  alert('Not yet, but you never know in the future! The answer is two.');
+} else if (daughters === 'no') {
+  /*console.log('You\'re a keen observer! That\'s right, I have two.');*/
+  alert('You\'re a keen observer! That\'s right, I have two.');
+} else {
+  /*console.log('You are a rebel, ain\'t ya?');*/
+  alert('You are a rebel, ain\'t ya?');
+}
+
+alert(username + ', I don\'t know how well you really know me, but you\'re in for now.');
