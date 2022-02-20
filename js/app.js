b76757c35dcdff8e6c6f7054c174880a4595671e
Derek Douglas <derek.j.douglas13@gmail.com>
2022-02-19 18:33:03 -0600

    Added 2 questions for quiz expansion and additional list of favorite cheeses

--- js/app.js
+++ js/app.js
@@ -1,74 +1,125 @@
 'use strict';
 
-/*console.log('hello world');*/
+console.log('hello world');
 
 
 let username = prompt('Hello there. What is your name?');
 alert('Ah, just as I thought; ' + username + '. A wonderful name!');
 
+//Question 1:
 let game = prompt('Would you like to play a game? All responses should be \'yes\' or \'no\'.').toLowerCase();
 
 if (game === 'yes') {
-  /*console.log('Eggselent');*/
+//console.log('Eggselent');
   alert('Eggselent');
 } else if (game === 'no') {
-  /*console.log('Too bad! We\'re playing anyway!');*/
+//console.log('Too bad! We\'re playing anyway!');
   alert('Too bad! We\'re playing anyway!');
 } else {
-  /*console.log('Hmm, I don\'t understand. Let\'s play anyways.');*/
+//console.log('Hmm, I don\'t understand. Let\'s play anyways.');
   alert('Hmm, I don\'t understand. Let\'s play anyways.');
 }
 
+//Question 2:
 let military = prompt('Was I in the Navy?').toLowerCase();
 
 if (military === 'yes') {
-  /*console.log('Nope. I was in the Air Force');*/
+//console.log('Nope. I was in the Air Force');
   alert('Nope. I was in the Air Force.');
 } else if (military === 'no') {
-  /*console.log('That\'s right! I was in the Air Force.');*/
+  //console.log('That\'s right! I was in the Air Force.');
   alert('That\'s right! I was in the Air Force.');
 } else {
-  /*console.log('I see you\'re playing hardball. Let\'s move on.');*/
+  //console.log('I see you\'re playing hardball. Let\'s move on.');
   alert('I see you\'re playing hardball. Let\'s move on.');
 }
 
+//Question 3:
 let hometown = prompt('Am I from Western New York?').toLowerCase();
 
 if (hometown === 'yes') {
-  /*console.log('That\'s right! From the southern shores of Lake Ontario.');*/
+  //console.log('That\'s right! From the southern shores of Lake Ontario.');
   alert('That\'s right! From the southern shores of Lake Ontario.');
 } else if (hometown === 'no') {
-  /*console.log('WHAT?! Do you even know me at all? Of course I am!');*/
+  //console.log('WHAT?! Do you even know me at all? Of course I am!');
   alert('WHAT?! Do you even know me at all? Of course I am!');
 } else {
-  /*console.log('Don\'t like to follow the rules? That\'s okay.');*/
+  //console.log('Don\'t like to follow the rules? That\'s okay.');
   alert('Don\'t like to follow the rules? That\'s okay.');
 }
 
+//Question 4:
 let peach = prompt('Did I play in peach orchards as a kid?').toLowerCase();
 
 if (peach === 'yes') {
-  /*console.log('I sure did. The climate of the great lakes helps to enable fruit tree orchards in such a cold climate.');*/
+  //console.log('I sure did. The climate of the great lakes helps to enable fruit tree orchards in such a cold climate.');
   alert('I sure did. The climate of the great lakes helps to enable fruit tree orchards in such a cold climate.');
-} else if (peach = 'no') {
-  /*console.log('Uhm, I actually did. Awkward.');*/
+} else if (peach === 'no') {
+  //console.log('Uhm, I actually did. Awkward.');
   alert('Uhm, I actually did. Awkward.');
 } else {
-  /*console.log('At a loss for words, eh?');*/
+  //console.log('At a loss for words, eh?');
   alert('At a loss for words, eh?');
 }
 
+//Question 5:
 let daughters = prompt('Do I have three daughters?').toLowerCase();
 
 if (daughters === 'yes') {
-  /*console.log('Not yet, but you never know in the future! The answer is two.');*/
+  //console.log('Not yet, but you never know in the future! The answer is two.');
   alert('Not yet, but you never know in the future! The answer is two.');
 } else if (daughters === 'no') {
-  /*console.log('You\'re a keen observer! That\'s right, I have two.');*/
+  //console.log('You\'re a keen observer! That\'s right, I have two.');
   alert('You\'re a keen observer! That\'s right, I have two.');
 } else {
-  /*console.log('You are a rebel, ain\'t ya?');*/
+  //console.log('You are a rebel, ain\'t ya?');
   alert('You are a rebel, ain\'t ya?');
 }
 
-alert(username + ', I don\'t know how well you really know me, but you\'re in for now.');
+//Question 6:
+//Where does swiss cheese rank on my Top 10 List of favorite cheeses? hint: 1-10
+
+//Define number of attempts
+let attempts = 4;
+
+//Define the answer
+let swiss = 6;
+
+for (let i = 0; i < attempts; i++) {
+  let favoriteCheese = prompt('Where does swiss rank on my top 10 list of favorite cheeses? You have four attempts! Hint: 1-10');
+  if (favoriteCheese === '6') {
+    console.log('That\'s correct!');
+    break;
+  }
+  if (favoriteCheese < swiss) {
+    console.log(`Just a little low! You have ${attempts - i - 1} attempts remaining.`);
+  }
+  if (favoriteCheese > swiss) {
+    console.log(`Too high! You have ${attempts - i - 1} attempts remaining.`);
+  }
+}
+
+//Question 7 Guessing game with an array
+//colors:  red, orange, yellow, green, blue, indigo, violet
+let rainbow = ['red','orange','yellow','blue','indigo','violet'];
+
+let j = 1;
+
+while (j < 7) {
+  let color = prompt('What is my favorite color of the rainbow?'); {
+    //console.log(color);
+    //console.log(rainbow[1]);
+    if (color === rainbow[1]) {
+      console.log('That is correct.');
+      break;
+    }
+    else {
+      console.log(`Nope, try again. You have ${6 - j} attempts remaining.`);
+      j++;
+    }
+  }
+}
+
+if (j > 6) {
+  alert('You have failed. There aren\'t even that many colors to pick from!');
+}
